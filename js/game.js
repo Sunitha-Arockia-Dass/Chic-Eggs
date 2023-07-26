2a113e05a103bcd2b165eac5468c94abb284ce99
“seetha1730” <seetharaj1990@gmail.com>
2023-07-26 19:11:13 +0200

    beginning of js

---
+++ js/game.js
@@ -0,0 +1,58 @@
+class Game{
+    constructor() {
+        this.startScreen = document.getElementById("start-screen")
+       this.gameScreen = document.getElementById("game-screen")
+       this.easyScreen = document.getElementById("easy-level")
+       this.mediumScreen = document.getElementById("medium-level")
+       this.hardScreen = document.getElementById("hard-level")
+       this.extremeScreen = document.getElementById("extreme-level")
+       
+    }
+
+    easy(){
+        this.mediumScreen.style.display = "none"
+        this.hardScreen.style.display = "none"
+        this.extremeScreen.style.display = "none"
+        this.easyScreen.style.display = "block"
+
+      this.startScreen.style.display = "none"
+      //this.gameScreen.style.display = "block"
+
+    }
+    medium(){
+        this.easyScreen.style.display = "none"
+        this.hardScreen.style.display = "none"
+        this.extremeScreen.style.display = "none"
+        this.mediumScreen.style.display = "block"
+
+      this.startScreen.style.display = "none"
+      this.gameScreen.style.display = "block"
+
+    }
+    hard(){
+        this.easyScreen.style.display = "none"
+        this.mediumScreen.style.display = "none"
+        this.extremeScreen.style.display = "none"
+       this.hardScreen.style.display = "block"
+
+      this.startScreen.style.display = "none"
+      this.gameScreen.style.display = "block"
+
+    }
+    extreme(){
+        this.easyScreen.style.display = "none"
+        this.mediumScreen.style.display = "none"
+        this.hardScreen.style.display = "none"
+        this.extremeScreen.style.display = "block"
+        
+      this.startScreen.style.display = "none"
+      this.gameScreen.style.display = "block"
+
+    }
+
+    
+
+}
+
+
+    
--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
-
-    const easyBtn = document.querySelector(".glossy-button--green");
-
-
-easyBtn.addEventListener("click", function () {
-console.log("easybtn")
-
-    })
-
+const game = new Game();
+window.onload = function () {
+  const easyBtn = document.querySelector(".button-green");
+  const mediumBtn = document.querySelector(".button-blue");
+  const hardBtn = document.querySelector(".button-gold");
+  const extremeBtn = document.querySelector(".button-red");
+// Easy button click
+this.gameScreen.style.display = "none"
+  easyBtn.addEventListener("click", function () {
+    game.easy();
+  });
+// Medium button click
+  mediumBtn.addEventListener("click", function () {
+    game.medium();
+  });
+// Hard button click
+  hardBtn.addEventListener("click", function () {
+    game.hard();
+  });
+// Extreme button click
+  extremeBtn.addEventListener("click", function () {
+    game.extreme();
+  });
+};
