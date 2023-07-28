13569c8c1c03d400be9f21f3dfb3393a7b1ead23
Sunitha-Arockia-Dass <135265573+Sunitha-Arockia-Dass@users.noreply.github.com>
2023-07-28 15:35:25 +0200

    game javascript

--- js/game.js
+++ js/game.js
@@ -6,50 +6,60 @@ class Game{
        this.mediumScreen = document.getElementById("medium-level")
        this.hardScreen = document.getElementById("hard-level")
        this.extremeScreen = document.getElementById("extreme-level")
-       
+       this.startScreen.style.display = "block"
+       this.gameScreen.style.display = "none"
+       this.easyScreen.style.display = "none"
+       this.mediumScreen.style.display = "none"
+       this.hardScreen.style.display = "none"
+       this.extremeScreen.style.display = "none"
+
     }
 
     easy(){
-        this.mediumScreen.style.display = "none"
-        this.hardScreen.style.display = "none"
-        this.extremeScreen.style.display = "none"
         this.easyScreen.style.display = "block"
-      this.startScreen.style.display = "none"
-      this.gameScreen.style.display = "block"
+        this.startScreen.style.display = "none"
+        this.gameScreen.style.display = "block"
 
     }
     medium(){
-        this.easyScreen.style.display = "none"
-        this.hardScreen.style.display = "none"
-        this.extremeScreen.style.display = "none"
-        this.mediumScreen.style.display = "block"
-
       this.startScreen.style.display = "none"
+      this.mediumScreen.style.display = "block"
       this.gameScreen.style.display = "block"
 
     }
     hard(){
-        this.easyScreen.style.display = "none"
-        this.mediumScreen.style.display = "none"
-        this.extremeScreen.style.display = "none"
-       this.hardScreen.style.display = "block"
-
       this.startScreen.style.display = "none"
       this.gameScreen.style.display = "block"
+      this.hardScreen.style.display = "block"
 
     }
     extreme(){
-        this.easyScreen.style.display = "none"
-        this.mediumScreen.style.display = "none"
-        this.hardScreen.style.display = "none"
-        this.extremeScreen.style.display = "block"
-        
       this.startScreen.style.display = "none"
       this.gameScreen.style.display = "block"
-
+      this.extremeScreen.style.display ="block"        
     }
+    generateRandomWord(words){
+      let randomNumber=Math.floor(Math.random()*words.length)
+      return words[randomNumber]
+    }
+    compareWords(word){
+      let randomWord=generateRandomWord(words)
+      let array=Array.from(randomWord)
+      word.forEach((letter,index)=>{
+        if(array.includes(letter)){
+          if(index===array.indexOf(letter) ){
+            moon++
+          }
+          else{
+            star++
+          }
+        }
+          else{
+            sun++
+          }
+      })
 
-    
+  }
 
 }
 
